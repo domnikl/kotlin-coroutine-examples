de21dc4f2fdf6abaa6e8df0d4d0646eea45a7729
Dominik Liebler <liebler.dominik@gmail.com>
2019-08-28 19:35:23 +0200

    initial commit

---
+++ src/main/kotlin/programmingKotlinFirst.kt
@@ -0,0 +1,23 @@
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() {
+    fun task1() {
+        println("start task1 in Thread ${Thread.currentThread()}")
+        println("end task1 in Thread ${Thread.currentThread()}")
+    }
+    fun task2() {
+        println("start task2 in Thread ${Thread.currentThread()}")
+        println("end task2 in Thread ${Thread.currentThread()}")
+    }
+
+    println("start")
+
+    runBlocking {
+        launch { task1() }
+        launch { task2() }
+        println("called task1 and task2 from ${Thread.currentThread()}")
+    }
+
+    println("done")
+}
