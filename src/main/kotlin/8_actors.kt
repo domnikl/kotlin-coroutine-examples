b44e6167ba151026a707ef30b5d8a3ccd65d75eb
Dominik Liebler <liebler.dominik@gmail.com>
2019-08-29 08:28:27 +0200

    example 8: simple actor

---
+++ src/main/kotlin/8_actors.kt
@@ -0,0 +1,30 @@
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.actor
+import kotlin.coroutines.CoroutineContext
+
+// this starts an actor that runs on the default dispatcher and records every message send to it
+// when it is closed, it will print all messages and stop
+
+@ObsoleteCoroutinesApi
+fun CoroutineScope.printActor(context: CoroutineContext) = actor<String>(context) {
+    val messages = mutableListOf<String>()
+
+    for (msg in channel) {
+        messages.add(msg)
+    }
+
+    println("from thread ${Thread.currentThread()}")
+    println(messages.joinToString(", "))
+}
+
+@ExperimentalCoroutinesApi
+@ObsoleteCoroutinesApi
+fun main()  = runBlocking<Unit> {
+    val actor = printActor(Dispatchers.Default)
+
+    repeat(100) { i ->
+        actor.send(i.toString())
+    }
+
+    actor.close()
+}
