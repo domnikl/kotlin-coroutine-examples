f1fa1ecf7cfb71b8d054c257cfbec5a37fa8437e
Dominik Liebler <liebler.dominik@gmail.com>
2019-08-29 07:19:39 +0200

    example 1: simple concurrency using launch()

---
+++ src/main/kotlin/1_concurrency.kt
@@ -0,0 +1,25 @@
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+// this launches 2 coroutines and runs them concurrently (but not in parallel)
+
+fun main() {
+    fun task1() {
+        println("start task1 in Thread ${Thread.currentThread()}")
+        println("end task1 in Thread ${Thread.currentThread()}")
+    }
+    fun task2() {
+        println("start task2 in Thread ${Thread.currentThread()}")
+        println("end task2 in Thread ${Thread.currentThread()}")
+    }
+
+    println("start")
+
+    runBlocking {
+        launch { task1() }
+        launch { task2() }
+        println("called task1 and task2 from ${Thread.currentThread()}")
+    }
+
+    println("done")
+}
